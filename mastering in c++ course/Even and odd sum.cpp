74ecfc38eada3d678bf25f62afb6ba3ec42e4dc6
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2022-07-18 04:02:58 +0200

    Create Even and odd sum.cpp

---
+++ mastering in c++ course/Even and odd sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n1, n2, n3, n4, n5, n6, n7, n8;
+	cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6 >> n7 >> n8;
+
+	cout << n1 + n3 + n5 + n7 ;
+	cout << " ";
+	cout << n2 + n4 + n6 + n8;
+
+	return 0;
+
+}
