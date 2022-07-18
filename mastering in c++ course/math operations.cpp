8bc3c6d74fc4b6a7cde415366ee256f2882eb153
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2022-07-18 03:57:12 +0200

    Create math operations.cpp

---
+++ mastering in c++ course/math operations.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout << "Enter first number: ";
+	cin >> a;
+
+	int b;
+	cout << "Enter seconed number: ";
+	cin >> b;
+
+	cout << a << " + " << b << " = " << a + b << endl; 
+	cout << a << " - " << b << " = " << a - b << endl;
+	cout << a << " / " << b << " = " << a / b << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+
+	return 0;
+
+}
