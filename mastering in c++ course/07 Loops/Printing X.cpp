7d0fc50c2f1848e6dae804108e48210d2845ccfa
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2022-07-18 15:10:53 +0200

    Create Printing X.cpp

---
+++ mastering in c++ course/07 Loops/Printing X.cpp
@@ -0,0 +1,22 @@
+/*
+● Read an Integer N, then print an X using *
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for (int i=0 ; i<n ; ++i){
+        for(int j=0 ; j<n ; ++j){
+            if( i==j || n-i-1==j)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
