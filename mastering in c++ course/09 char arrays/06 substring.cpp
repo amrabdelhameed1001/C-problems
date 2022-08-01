aa6fcdb7c65de36aa70c25086fd26904e126e998
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2022-08-01 12:16:09 +0200

    Create 06 substring.cpp

---
+++ mastering in c++ course/09 char arrays/06 substring.cpp
@@ -0,0 +1,28 @@
+// Read 2 strings input and str. Print YES if the given str is a substring for the string. Otherwise, print NO
+#include <iostream>
+using namespace std;
+
+int main(){
+    string a , b;
+    cin >> a >> b;
+
+    int idx0;
+    for(int i=0 ; i<a.size() ; i++)
+        if(b[0] == a[i]) {
+            idx0 = i;
+            break;
+        }
+
+    bool substring=true;
+    for(int i=1 ; i<b.size() ; i++){
+        if(b[i] != a[idx0+i]){
+            substring = false;
+            break;
+        }
+    }
+
+    if(substring)   cout << "YES";
+    else            cout << "NO";
+
+    return 0;
+}
