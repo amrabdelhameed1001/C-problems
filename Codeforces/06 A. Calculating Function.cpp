b38c22d002283030d359500b5d575b2eb0b2d6fb
amrabdelhameed1001 <85768785+amrabdelhameed1001@users.noreply.github.com>
2022-09-19 06:38:35 +0200

    Create 06 A. Calculating Function.cpp

---
+++ Codeforces/06 A. Calculating Function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    long long n;
+    cin >> n;
+
+    if(n %2==0){
+        cout << n/2;
+    }
+    else
+        cout << -1 * (n/2) -1;
+    return 0;
+}
